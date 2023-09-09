519e395f98f3a4f57098bb68d02fba4e21f0193c
neel2435 <neeldoshi2435@gmail.com>
2023-09-09 15:12:41 -0700

    corrected About Page

--- src/lib/Titlebar.svelte
+++ src/lib/Titlebar.svelte
@@ -3,52 +3,101 @@
 </script>
 
 <div class="topnav">
-    <img src="/whitecomp.png" alt="site logo">
+    <img src="/whitecomp.png" alt="site logo" class="logo">
     <ul>
         <li><a href="/">Home</a></li>
-        <li><a href="https://www.linkedin.com/in/neel-doshi-5356a3205/">Linkedin</a></li>
-        <li><a href="https://github.com/neel2435">Github</a></li>
+        <li>
+          <div class="icon-wrapper">
+            <a href="https://www.linkedin.com/in/neel-doshi-5356a3205/" class="icon-link">
+              <img src="/icons8-linkedin-50.png" alt="LinkedIn" class="icon-linked" />
+            </a>
+          </div>
+        </li>
+        <li>
+          <div class="icon-wrapper">
+            <a href="https://github.com/neel2435" class="icon-link">
+              <img src="/icons8-github-50.png" alt="GitHub" class="icon-git" />
+            </a>
+          </div>
+        </li>
         <li><a href="/about">About</a></li>
     </ul>
 </div>
 
 <style>
 .topnav {
-  overflow: hidden;
   background-color: rgb(0, 0, 0);
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    height: 80px;
-    display: flex;
-    align-items: center;
-    box-shadow: 0 0 25px 0 rgb(0, 0, 0);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  box-shadow: 0 0 25px 0 rgb(0, 0, 0);
 }
 
-.topnav * {
-    display: inline;
+.topnav ul {
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+}
+
+.topnav li {
+  margin: 0 30px;
+  list-style: none;
+}
+
+.topnav a {
+  color: rgb(255, 255, 255);
+  transition: color 0.5s ease;
+  text-decoration: none;
+  font-size: 1rem;
+  font-family: 'Lucida Sans';
+  vertical-align: middle;
+}
+
+.topnav .icon-wrapper {
+  display: inline-block;
+  transition: background-color 0.5s ease;
 }
 
 .topnav a:hover {
   color: rgb(99, 236, 154);
 }
 
+.icon-wrapper:hover .icon-git {
+    content: url('/icons8-github-51.png')
+}
+
+.icon-wrapper:hover .icon-linked {
+    content:url('/icons8-linkedin-51.png')
+}
+
+.icon-git {
+  display: block;
+  width: 25px;
+  height: 25px;
+  filter: none;
+}
+
+.icon-linked {
+  display: block;
+  width: 25px;
+  height: 25px;
+  filter: none;
+}
+
 .topnav img {
-    height: 25px;
+  vertical-align: middle;
+}
+
+.topnav .logo {
     width: 25px;
+    height: 25px;
     padding: 16px 16px;
-}
-
-.topnav li {
-    margin: 30px;
-}
-
-.topnav li a {
-    color: rgb(255, 255, 255);
-    transition: color 0.5s ease;
-    text-decoration: none;
-    font-size: 1rem;
-    font-family:'Lucida Sans';
+    padding-left: 35px;
 }
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,73 +6,101 @@
 <Titlebar />
 <MatrixRain />
 <body>
-    <div class="about">
-    
-    <h1 style= "font-size: 50px">About Me</h1>
-    
-    <p style="font-size: 24px; padding: 15px; margin: 0px;">
-        Welcome to my website! I graduated from the 
-        <span style="color: blue;">University</span>
-        <span style="color: yellow;">of</span>
-        <span style="color: blue;">California</span>,
-        <span style="color: yellow;">Riverside</span> 
-        in June 2023 with a degree in Computer Science.<br><br>
-        I'm looking for careers in the following spaces:<br><br><br>
-    </p>
-      
-    <p style="
-    font-size:35px;
-    padding:15px;
-    margin:0px;">
-    <span style="color: black;">Data</span> 
-    Analysis/Engineering, 
-    <span style="color: black;">Software</span> 
-    Engineering/Development, 
-    <span style="color: black;">Cybersecurity</span>,
-    and 
-    <span style="color: black;">Database</span> 
-    Administration/Engineering
-    </p>
-    
+    <div class = "wrapper">
+    <div class="container">
+        <div class="about">
+            <h1>About Me</h1>
+            <p>
+                Welcome to my website! I graduated from the 
+                <span class="color-blue">University</span>
+                <span class="color-yellow">of</span>
+                <span class="color-blue">California</span>,
+                <span class="color-yellow">Riverside</span> 
+                in June 2023 with a degree in Computer Science.
+            </p>
+            <p>
+                I'm looking for careers in the following spaces:
+            </p>
+            <p class="career">
+                Data Analysis/Engineering, 
+                Software Engineering/Development, 
+                Cybersecurity,
+                and 
+                Database Administration/Engineering
+            </p>
+        </div>
+    </div>
     </div>
 </body>
+
+
     
 <style>
-        body {
-            position: relative;
-            margin: 0 auto;
-            top: 200px;
-            width: 1400px;
-            height: 580px;
-            overflow: auto;
-            background:linear-gradient(rgb(26, 29, 27), rgba(110, 211, 144, 1));
-            opacity: 0;
-            animation: fade-in 1.5s ease forwards;
-            border-radius: 20px;
-            border: 5px solid white;
-            box-sizing: border-box;
-        }
-    
-        @keyframes fade-in {
-            from {
-                opacity: 0; 
-                transform: translateY(30%);
-            }
-            to {
-                opacity: 1;
-                transform: translateY(0);
-            }
-        }
-    
-        .about {
-            display: block;
-            padding-top: 35px;
-            padding-left: 100px;
-            padding-right: 100px;
-            margin: 0 auto; 
-            color: white;
-            text-align: center; 
-            font-family: monospace;
-        }
+.wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+
+.container {
+    margin: -5% auto 0;
+    padding: 0;
+    background: linear-gradient(rgb(26, 29, 27), rgba(110, 211, 144, 1));
+    animation: fade-in 1.5s ease forwards;
+    border-radius: 20px;
+    box-sizing: border-box;
+    text-align: center;
+    width: 100%;
+    max-width: 1400px;
+    min-width: 320px; /* Set min-width to a reasonable value */
+}
+
+@keyframes fade-in {
+    from {
+        opacity: 0; 
+        transform: translateY(30%);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.about {
+    padding: 2vw; /* Adjust as needed */
+    color: white;
+    text-align: center; 
+    font-family: monospace;
+    word-wrap: break-word;
+}
+
+h1 {
+    font-size: 3vw; /* Adjust as needed */
+}
+
+p {
+    font-size: 1.5vw; /* Adjust as needed */
+    padding: 1vw; /* Adjust as needed */
+    margin: 0;
+}
+
+p.career {
+    font-size: 2vw; /* Adjust as needed */
+}
+
+.color-blue {
+    color: blue;
+}
+
+.color-yellow {
+    color: yellow;
+}
+
+body {
+    overflow-y: hidden;
+    overflow-x: hidden;
+}
     
 </style>
